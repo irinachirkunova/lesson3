3144895be10351acd7fb66215eb1bc79a512ffde
Irina Chirkunova <irina.chirkunova86@gmail.com>
2019-07-04 23:40:44 +0200

    Creation lesson04Dif

--- scripts/script.js
+++ scripts/script.js
@@ -1,65 +1,23 @@
 'use scrict';
-let money = +prompt('Ваш месячный доход?'),
-    income = "freelance",
-    mission = 30000,
-    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую:'),
-    deposit = confirm('Есть ли у вас депозит в банке?'),
-    question1 = prompt("Какие обязательные ежемесячные расходы у вас есть?"),
-    question2 = +prompt("Во сколько это обойдется?"), 
-    question3 = prompt("Какие обязательные ежемесячные расходы у вас есть?"),
-    question4 = +prompt("Во сколько это обойдется?"); 
-
-let budgetMonth = money - question2 - question4;
-
-let period = mission / budgetMonth;
-
-let budgetDay = budgetMonth / 30;
-
-
-let showTypeof = function(item) {
-    console.log(item, typeof item);
-}
-
-showTypeof(money);
-showTypeof(income);
-showTypeof(deposit); 
-
-function getStatusIncome() {
-    if(budgetDay > 800) {
-    return ("Высокий уровень доход");
-    }
-    else if(budgetDay > 300 && budgetDay <= 800) {
-    return ("Средний уровень дохода");
-    }
-    else if(budgetDay >= 0 && budgetDay <= 300){
-    return ("Низкий уровень дохода");
-    }
-    else {
-    return ("Что то пошло не так");
-    }    
-}
-
-console.log("getStatusIncome(): ", getStatusIncome());
  
-//1-a
-let getExpensesMonth = function() {
-    return question2 + question4
-}
+ let changeString = function(a) {   
+     if(!isNaN(a)) {
+         return ("В  аргумент передана не строка!");
+     }
+     else if(a.length < 30) {
+         return a = a.trim();
+     }
+     else {
+         return a = a.slice(0,29) + "...";
 
-console.log("Cуммa всех расходов за месяц: ", getExpensesMonth());
+     }
+ }
 
-//1-b
-function getAccumulatedMonth() {
-    return money - getExpensesMonth();
-}
+ let a = 10;
+ console.log(changeString(a));
 
-let accumulatedMonth = getAccumulatedMonth();
-console.log("Накопления за месяц: " + accumulatedMonth); 
+ let b = "   Если строка более 30  "
+ console.log(changeString(b));
 
-//1-c
-function getTargetMonth() 
-{
-    return Math.ceil(mission/ getAccumulatedMonth());
-}
-console.log("за какой период будет достигнута цель: " + getTargetMonth());
- 
+ let c = "Если строка более 30 знаков, то после 30го символа часть текста скрывается и вместо них появляются три точки";
+ console.log(changeString(c));
