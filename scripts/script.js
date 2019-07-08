5b2bc6e54de2286d2b44f63900e8a69945c2fd85
Irina Chirkunova <irina.chirkunova86@gmail.com>
2019-07-08 17:59:41 +0200

    Creation lesson05Dif

--- scripts/script.js
+++ scripts/script.js
@@ -1,65 +1,24 @@
 'use scrict';
-let money = +prompt('Ваш месячный доход?'),
-    income = "freelance",
-    mission = 30000,
-    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую:'),
-    deposit = confirm('Есть ли у вас депозит в банке?'),
-    question1 = prompt("Какие обязательные ежемесячные расходы у вас есть?"),
-    question2 = +prompt("Во сколько это обойдется?"), 
-    question3 = prompt("Какие обязательные ежемесячные расходы у вас есть?"),
-    question4 = +prompt("Во сколько это обойдется?"); 
 
-let budgetMonth = money - question2 - question4;
-
-let period = mission / budgetMonth;
-
-let budgetDay = budgetMonth / 30;
-
-
-let showTypeof = function(item) {
-    console.log(item, typeof item);
-}
-
-showTypeof(money);
-showTypeof(income);
-showTypeof(deposit); 
-
-function getStatusIncome() {
-    if(budgetDay > 800) {
-    return ("Высокий уровень доход");
+//1
+function showArrayOfNumbers(array) {
+    for(let i = 0; i < array.length; i++) {
+        if(array[i].charAt(0) == '2' || array[i].charAt(0) == '4') {
+            console.log(array[i]);
+        }
     }
-    else if(budgetDay > 300 && budgetDay <= 800) {
-    return ("Средний уровень дохода");
+}
+let array = ['24', '42', '89', '77', '44', '20', '29'];
+showArrayOfNumbers(array);
+
+
+//2
+function showSimpleNumbers() {
+    for (var i = 2; i <= 100; i++){
+        for(var j = 2; j <= i; j++){
+            if (i % j == 0) break;
+        }
+        if(j == i) console.log("Простое число: " + i + " Делителя данного числа: 1, " + j);
     }
-    else if(budgetDay >= 0 && budgetDay <= 300){
-    return ("Низкий уровень дохода");
-    }
-    else {
-    return ("Что то пошло не так");
-    }    
 }
-
-console.log("getStatusIncome(): ", getStatusIncome());
- 
-//1-a
-let getExpensesMonth = function() {
-    return question2 + question4
-}
-
-console.log("Cуммa всех расходов за месяц: ", getExpensesMonth());
-
-//1-b
-function getAccumulatedMonth() {
-    return money - getExpensesMonth();
-}
-
-let accumulatedMonth = getAccumulatedMonth();
-console.log("Накопления за месяц: " + accumulatedMonth); 
-
-//1-c
-function getTargetMonth() 
-{
-    return Math.ceil(mission/ getAccumulatedMonth());
-}
-console.log("за какой период будет достигнута цель: " + getTargetMonth());
- 
+showSimpleNumbers();
